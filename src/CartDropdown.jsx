79eb59c42d5057f8bfa2eca6b425e1289c446140
Engi Lozada <engilozada@gmail.com>
2023-02-16 14:59:39 +0100

    Add AddToCart feature

--- src/CartDropdown.jsx
+++ src/CartDropdown.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import { useContext } from "react";
+
+import { CartContext } from "./contexts/CartContext";
 
 import Button from "./Button";
+import CartItem from "./CartItem";
 
 import "./CartDropdown.css";
 
-const CartDropdown = () => (
-  <div className="cart-dropdown-container">
-    <div className="cart-items" />
-    <Button>CHECKOUT</Button>
-  </div>
-);
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  console.log(cartItems);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <Button>CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
--- src/CartIcon.jsx
+++ src/CartIcon.jsx
@@ -5,13 +5,13 @@ import { ReactComponent as ShoppingIcon } from "./assets/shopping-bag.svg";
 import "./CartIcon.css";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <div className="cart-icon-container">
       <ShoppingIcon className="shopping-icon" onClick={toggleIsCartOpen} />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartItemCount}</span>
     </div>
   );
 };
