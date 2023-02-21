002eacd3b7e9011d3e4259de03222aff7aa75c74
Engi Lozada <engilozada@gmail.com>
2023-02-21 23:33:02 +0100

    Fix home route

--- src/CategoryItem.jsx
+++ src/CategoryItem.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./CategoryItem.css";
 
-const CategoryItem = ({ title, url }) => {
+const CategoryItem = ({ title, url, route }) => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate(route);
+  };
+
   return (
     <div className="Category-container">
       <div
         className="Category-image"
         style={{ backgroundImage: `url(${url})` }}
       />
-      <div className="Category-text-container">
-        <h2>{title}</h2>
+      <div className="Category-text-container" onClick={navigateHandler}>
+        <h2>{title.toUpperCase()}</h2>
         <p>Shop Now</p>
       </div>
     </div>
--- src/CategoryList.jsx
+++ src/CategoryList.jsx
@@ -1,12 +1,53 @@
 import React from "react";
+
 import CategoryItem from "./CategoryItem";
 import "./CategoryList.css";
 
-const CategoryList = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: "shop/hats",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route: "shop/sneakers",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: "shop/womens",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: "shop/mens",
+  },
+];
+
+const CategoryList = () => {
   return (
     <div className="Category-list-container">
       {categories.map((c) => {
-        return <CategoryItem key={c.id} title={c.title} url={c.imageUrl} />;
+        return (
+          <CategoryItem
+            key={c.id}
+            title={c.title}
+            url={c.imageUrl}
+            route={c.route}
+          />
+        );
       })}
     </div>
   );
